0a67e7ae33775158a28522e55b412757a8b6330f
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-11-12 19:28:07 +0530

    feat: add utils Api Error|Response class, asynchandler

--- backend/src/utils/ApiResponse.ts
+++ backend/src/utils/ApiResponse.ts
@@ -1,6 +1,9 @@
 export class ApiResponse {
   public success: boolean
-  constructor(public statusCode: number, public message: string, public data: any) {
+  constructor(
+    public statusCode: number,
+    public message: string,
+    public data: any) {
     this.success = statusCode < 400
   }
 }
---
+++ backend/src/utils/asyncHandler.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction, RequestHandler } from "express";
+
+const asyncHandler =
+  (handler: (req: Request, res: Response, next: NextFunction) => Promise<any>): RequestHandler =>
+    (req, res, next) => {
+      Promise.resolve(handler(req, res, next)).catch(next);
+    };
+
+export default asyncHandler;

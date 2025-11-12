d071c5f2729f4ef20b1ee065222e9d39c850cbe4
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-11-12 19:18:55 +0530

    init: setup project

---
+++ backend/src/utils/ApiResponse.ts
@@ -0,0 +1,6 @@
+export class ApiResponse {
+  public success: boolean
+  constructor(public statusCode: number, public message: string, public data: any) {
+    this.success = statusCode < 400
+  }
+}

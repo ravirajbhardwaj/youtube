0c341ffb18004a71de72c3cdf672edea101b6cca
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-11-16 19:42:07 +0530

    feat: add application's healthcheck

---
+++ backend/src/api/healthcheck.ts
@@ -0,0 +1,12 @@
+import { Router, Request, Response } from "express";
+import { ApiResponse } from "../utils/ApiResponse";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Health check passed", null));
+})
+
+export default router;

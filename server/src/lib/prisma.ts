5fe076e8cc487f0c99e99626fd8dee029dafec9c
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-12-05 23:27:18 +0530

    feat: add Prisma client setup and connection logic

---
+++ server/src/lib/prisma.ts
@@ -0,0 +1,31 @@
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
+import { env } from './env'
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL })
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter })
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+export const prisma = globalForPrisma.prisma || prismaClientSingleton()
+
+if (env.APP_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export async function connectPrisma() {
+  try {
+    await prisma.$connect()
+    console.info('Prisma connected to the database')
+  } catch (error) {
+    console.error(`Prisma failed to connect to the database ${error}`)
+    process.exit(1)
+  }
+}
+
+connectPrisma()

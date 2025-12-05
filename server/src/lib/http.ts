12147e19687b6d5677e2e9a8a3cffdb95365633d
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-12-06 01:51:02 +0530

    feat: implement ApiResponse and ApiError classes for standardized API responses

---
+++ server/src/lib/http.ts
@@ -0,0 +1,47 @@
+import { HttpStatus } from '@/lib/const'
+import { Context } from 'hono'
+
+export class ApiResponse<T = any> {
+  success: boolean
+  constructor(
+    public statusCode: number,
+    public data: T,
+    public message: string
+  ) {
+    this.success = statusCode >= 200 && statusCode < 400
+  }
+}
+
+export class ApiError extends Error {
+  success: boolean
+  data: null
+  statusCode: number
+
+  constructor(
+    statusCode: number,
+    message = 'Something went wrong',
+    stack = ''
+  ) {
+    super(message)
+    this.statusCode = statusCode
+    this.data = null
+    this.success = false
+    this.message = message
+
+    if (stack) {
+      this.stack = stack
+    } else {
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
+export const ok = <T>(c: Context, data: T, message = 'Success') =>
+  c.json(new ApiResponse(HttpStatus.OK, data, message), 200)
+
+export const created = <T>(c: Context, data: T, message = 'Created') =>
+  c.json(new ApiResponse(HttpStatus.CREATED, data, message), 201)
+
+export const error = (status: number, message: string) => {
+  throw new ApiError(status, message)
+}

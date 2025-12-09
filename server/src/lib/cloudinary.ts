9f8dff2d0200f8f6d2465a7df19798c5e95b4dab
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-12-09 16:43:45 +0530

    feat: add uploadToCloudinary function for file uploads to Cloudinary

---
+++ server/src/lib/cloudinary.ts
@@ -0,0 +1,32 @@
+import { ApiError } from "./http";
+import { HttpStatus } from "./const";
+import { env } from "./env";
+
+
+export const uploadToCloudinary = async (file: File) => {
+  try {
+    const cloudName = env.CLOUDINARY_CLOUD_NAME
+    const uploadPreset = env.CLOUDINARY_UPLOAD_PRESET
+
+    const url = `https://api.cloudinary.com/v1_1/${cloudName}/auto/upload`
+
+    const form = new FormData()
+    form.append("file", file)
+    form.append("upload_preset", uploadPreset)
+
+    const res = await fetch(url, {
+      method: "POST",
+      body: form,
+    })
+
+    const body = await res.json()
+
+    if (!res.ok) {
+      throw new Error(body?.error?.message ?? "Cloudinary upload failed")
+    }
+
+    return body
+  } catch (error) {
+    throw new ApiError(HttpStatus.BAD_REQUEST, "Failed to upload on cloudinary")
+  }
+}

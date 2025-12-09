0c285694eb7dd8bb889e81bc9db9cb88ed3026ea
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-12-09 17:11:46 +0530

    refactor: remove generateTemporaryToken fn

--- server/src/lib/helper.ts
+++ server/src/lib/helper.ts
@@ -7,31 +7,3 @@ export const passwordMatch = async (
   enteredPassword: string,
   storedPassword: string
 ) => await Bun.password.verify(enteredPassword, storedPassword)
-
-const encoder = new TextEncoder()
-
-function toHex(buffer: ArrayBuffer | Uint8Array) {
-  const bytes = buffer instanceof Uint8Array ? buffer : new Uint8Array(buffer)
-  return Array.from(bytes)
-    .map(b => b.toString(16).padStart(2, '0'))
-    .join('')
-}
-
-export async function createHash(token: string) {
-  const hashBuffer = await crypto.subtle.digest(
-    'SHA-256',
-    encoder.encode(token)
-  )
-  return toHex(hashBuffer)
-}
-
-export async function generateTemporaryToken() {
-  const array = new Uint8Array(32)
-  crypto.getRandomValues(array)
-
-  const unHashedToken = toHex(array)
-  const hashedToken = await createHash(unHashedToken)
-  const tokenExpiry = new Date(Date.now() + USER_TEMPORARY_TOKEN_EXPIRY)
-
-  return { unHashedToken, hashedToken, tokenExpiry }
-}

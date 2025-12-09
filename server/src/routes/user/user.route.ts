8b917c8535ef3baab632cca1d8bd60aa27970eb2
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-12-09 13:58:33 +0530

    refactor: reorganize user routes by removing unused endpoints and improving structure

--- server/src/routes/user/user.route.ts
+++ server/src/routes/user/user.route.ts
@@ -3,33 +3,25 @@ import { Hono } from 'hono'
 const userRouter = new Hono()
 
 import {
-  changeCurrentPassword,
-  forgotPasswordRequest,
-  getCurrentUser,
-  getUserChannelProfile,
-  getWatchHistory,
   loginUser,
   logoutUser,
-  refreshAccessToken,
   registerUser,
-  resendEmailVerification,
-  resetForgottenPassword,
-  updateAccountDetails,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
   updateUserAvatar,
   updateUserCoverImage,
-  verifyEmail,
+  getUserChannelProfile,
+  getWatchHistory,
+  updateAccountDetails
 } from './user.controller'
 
 // Public routes
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
-userRouter.post('/forgot-password', forgotPasswordRequest)
-userRouter.post('/reset-password/:resetToken', resetForgottenPassword)
-userRouter.get('/verify-email/:verificationToken', verifyEmail)
 
 // Secured routes
 userRouter.post('/logout', logoutUser)
-userRouter.post('/resend-email-verification', resendEmailVerification)
 userRouter.post('/refresh-token', refreshAccessToken)
 userRouter.post('/change-password', changeCurrentPassword)
 userRouter.get('/current-user', getCurrentUser)

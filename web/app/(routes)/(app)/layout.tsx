ea7ae67b58215bb3f51a32fd67ae06025086aa57
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-12-12 16:17:18 +0530

    feat: add Suspense for dynamic content and make layout well

--- web/app/(routes)/(app)/layout.tsx
+++ web/app/(routes)/(app)/layout.tsx
@@ -1,4 +1,6 @@
 import Header from "./_components/Header";
+import Sidebar from "./_components/Sidebar";
+import { Suspense } from "react";
 
 export default function MainLayout({
   children,
@@ -8,7 +10,10 @@ export default function MainLayout({
   return (
     <div className="h-screen overflow-y-auto bg-background text-foreground">
       <Header />
-      {children}
+      <div className="flex min-h-[calc(100vh-66px)] sm:min-h-[calc(100vh-82px)] bg-background text-foreground">
+        <Sidebar />
+        <Suspense fallback={""}>{children}</Suspense>
+      </div>
     </div>
   );
 }

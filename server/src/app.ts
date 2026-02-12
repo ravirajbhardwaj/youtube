199a93dd9842f872fa7cdb6f2c62ded6ce3d3f0c
RaviRajBhardwaj <exploringwith.ravirajbhar@gmail.com>
2026-02-13 01:23:11 +0530

    feat: add main and sever starting file

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,22 +2,10 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { env } from '@/lib/env'
 import { secureHeaders } from 'hono/secure-headers'
-/**
- * IMPORT ROUTES
- */
-import { healthCheck } from './routes/health'
-import userRouter from './routes/user/user.route'
-import tweetRouter from './routes/tweet/tweet.route'
-import subscriptionRouter from './routes/subscription/subscription.route'
-import videoRouter from './routes/video/video.route'
-import commentRouter from './routes/comment/comment.route'
-import likeRouter from './routes/like/like.route'
-import playlistRouter from './routes/playlist/playlist.route'
-import dashboardRouter from './routes/dashboard/dashboard.route'
-import { Prisma } from './generated/prisma/client'
-import { ApiError } from './lib/http'
+import { ApiError, ApiResponse } from './lib/http'
+import { DrizzleError } from 'drizzle-orm'
 
-const app = new Hono()
+const app = new Hono({ strict: false }).basePath('/api/v1')
 
 const allowsOrigins = [env.CLIENT_URL, 'http://localhost:3000']
 
@@ -34,20 +22,41 @@ app.use(
   })
 )
 
-app.route('/api/v1/healthcheck', healthCheck)
-app.route('/api/v1/users', userRouter)
-app.route('/api/v1/tweets', tweetRouter)
-app.route('/api/v1/subscriptions', subscriptionRouter)
-app.route('/api/v1/videos', videoRouter)
-app.route('/api/v1/comments', commentRouter)
-app.route('/api/v1/likes', likeRouter)
-app.route('/api/v1/playlist', playlistRouter)
-app.route('/api/v1/dashboard', dashboardRouter)
+/**
+ * IMPORT ROUTES
+ */
+import authRouter from './routes/auth.route'
+import userRouter from './routes/user.route'
+import tweetRouter from './routes/tweet.route'
+import subscriptionRouter from './routes/subscription.route'
+import videoRouter from './routes/video.route'
+import commentRouter from './routes/comment.route'
+import likeRouter from './routes/like.route'
+import playlistRouter from './routes/playlist.route'
+import dashboardRouter from './routes/dashboard.route'
+import { HttpStatus } from './lib/const'
 
-// @ts-ignore
+app.route('/auth', authRouter)
+app.route('/users', userRouter)
+app.route('/tweets', tweetRouter)
+app.route('/subscriptions', subscriptionRouter)
+app.route('/videos', videoRouter)
+app.route('/comments', commentRouter)
+app.route('/likes', likeRouter)
+app.route('/playlist', playlistRouter)
+app.route('/dashboard', dashboardRouter)
+
+app.get('/healthcheck', c =>
+  c.json(new ApiResponse(HttpStatus.OK, null, 'Health Check Passed'))
+)
+
+app.notFound(c =>
+  c.json(new ApiResponse(HttpStatus.NOT_FOUND, null, 'RESOURCE NOT FOUND'))
+)
+
 app.onError((err, c) => {
   let apiError: ApiError
-  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+  if (err instanceof DrizzleError) {
     apiError = new ApiError(400, 'DATABASE ERROR')
   } else if (err instanceof ApiError) {
     apiError = err
@@ -60,7 +69,7 @@ app.onError((err, c) => {
       ...apiError,
       message: apiError.message,
     },
-    apiError.statusCode! as any
+    apiError.statusCode as any
   )
 })
 
